615c11287b459618314214e1e7a9ad8005be9ba6
Ademolu David <70884908+gentleprince@users.noreply.github.com>
2023-02-10 00:05:11 +0100

    Improved performance and stability

--- JS/module.js
+++ JS/module.js
@@ -1,15 +1,18 @@
-// JS Components
+//Importing JS Components
 
 // Bootstrap modification file
-import toggler from "./components/bs_bootstrap.js";
+import toggler from './components/bs_bootstrap.min.js';
 // Form Validations
-import * as func from "./components/validation.js";
+import * as func from './components/validation.min.js';
 // All animations
-import { onloadanim, onpagescroll } from "./components/animation.js";
+import * as animations from './components/animation.min.js';
 // Api data (json)
-import * as data from "./components/data.js";
+import * as data from './components/data.min.js';
 // testimonial arrow scroll
-import { testimonialScroll } from "./components/scroll.js";
+import {
+    testimonialScroll
+} from './components/scroll.min.js';
+
 
 // for bootstrap hamburger
 toggler.event;
@@ -18,18 +21,17 @@ toggler.event;
 func.validate();
 
 // animations
-// when scrolling
-onpagescroll();
+animations.default();
 
 // when page is loaded
 // if (window.location.href == 'http://127.0.0.1:5500/404.html') {
 //     loaded();
 //     async function loaded() {
-//         let errorPageOnLoadAnims = await import('./components/animation404.js',headers: {'Access-Control-Allow-Origin': '*'});
+//         let errorPageOnLoadAnims = await import('./components/animation404.min.js');
 //         errorPageOnLoadAnims.default();
 //     }
 // } else {
-window.window.onload = () => onloadanim;
+window.window.onload = () => animations.onloadanim;
 // }
 
 // olyxia json data & testimonial scrolling
